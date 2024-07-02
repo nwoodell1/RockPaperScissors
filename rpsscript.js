9742358e45dfaac06181b6dd0e8855330e83f870
nwoodell1 <nathan.woodell@gmail.com>
2024-07-01 20:34:28 -0500

    have everything we need to play a round. First we set the PC choice, then the user choice, and compare them to find a round winner.

--- rpsscript.js
+++ rpsscript.js
@@ -6,7 +6,45 @@ function getComputerChoice() {
         computerchoice = "rock";
     } else if (randomnumber < 0.66) { computerchoice = "paper"; }
     else { computerchoice = "scissors"; }
-    console.log(computerchoice)
+    console.log("comp says " + computerchoice)
+
+    return computerchoice
 }
 
-getComputerChoice()
+
+
+function getHumanChoice() {
+    var humanchoice = prompt("Rock, Paper, or Scissors?")
+
+
+    console.log("human says " + humanchoice)
+
+    return humanchoice
+}
+
+
+var humanscore = 0
+var computerscore = 0
+
+
+
+function playround(x, y) {
+    /*tie*/
+    if (x === y) { alert("Its a tie") }
+    /*human wins*/
+    else if (
+        (x === "rock" && y === "scissors") ||
+        (x === "paper" && y === "rock") ||
+        (x === "scissors" && y === "paper")
+    ) { alert(x + " beats " + y + ", Human wins this round"); humanscore++ }
+    /*comp wins*/
+    else { alert(y + " beats " + x + " , Comp wins this round"); computerscore++ }
+
+};
+
+const userplay = getHumanChoice()
+const compplay = getComputerChoice()
+
+playround(userplay, compplay)
+console.log("human score " + humanscore)
+console.log("comp score " + computerscore)
